127b38d4d32751b1f5289d4995f1c8ad385fdec5
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-10-08 13:38:13 -0400

    set up routes inside the routes folder

--- server.js
+++ server.js
@@ -31,60 +31,7 @@ db.on("error", function (error) {
 // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/redditScrape";
 
 // mongoose.connect(MONGODB_URI);
-app.get("/", function (req, res) {
-    res.render("index")
-});
-
-app.get("/all", function (req, res) {
-    // Find all results from the scrapedData collection in the db
-    db.article.find({}, function (error, found) {
-        // Throw any errors to the console
-        if (error) {
-            console.log(error);
-        }
-        // If there are no errors, send the data to the browser as json
-        else {
-            res.json(found);
-        }
-    });
-});
-
-
-app.get("/scrape", function (req, res) {
-    // Make a request via axios for the article section of reddit
-    axios.get("https://www.nytimes.com/section/world").then(function (response) {
-        // Load the html body from axios into cheerio
-        var $ = cheerio.load(response.data);
-        // For each element with a "title" class
-        $("h2").each(function (i, element) {
-            // Save the text and href of each link enclosed in the current element
-            var title = $(element).children().text();
-            var link = $(element).find("a").attr("href");
-
-            // If this found element had both a title and a link
-            if (title && link) {
-                // Insert the data in the scrapedData db
-                db.article.insert({
-                    title: title,
-                    link: link
-                },
-                    function (err, inserted) {
-                        if (err) {
-                            // Log the error if one is encountered during the query
-                            console.log(err);
-                        }
-                        else {
-                            // Otherwise, log the inserted data
-                            console.log(inserted);
-                        }
-                    });
-            }
-        });
-    });
-
-    // Send a "Scrape Complete" message to the browser
-    res.send("Scrape Complete");
-});
+require("./routes/routes.js")(app)
 // Listen on port 3000
 app.listen(3000, function () {
     console.log("App running on port 3000!");
