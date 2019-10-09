701cb26a368693ef28e21155bd7e4139c2a67953
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-10-09 18:24:45 -0400

    fixed the link

--- public/index.js
+++ public/index.js
@@ -11,7 +11,7 @@ $(function () {
             // $("#display").append(div)
             var div = $("<div>").attr("class", "card").css("width", "89rem").append(
                 $("<ul>").attr("class", "list-group list-group-flush"),
-                $("<a>").attr("class", "list-group list-group-flush").attr("href", "nytimes.com" + article.link).text(article.title),
+                $("<a>").attr("href", "http://nytimes.com" + article.link).attr("target", "_blank").text(article.title).css("background-color", "steelblue"),
                 // $("<li>").attr("class", "list-group-item").text(article.link),
                 $("<li>").attr("class", "list-group-item").text(article.snip),
             )
